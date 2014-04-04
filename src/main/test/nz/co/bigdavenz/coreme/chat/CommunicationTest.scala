6baaed894f07510bb33a703a0b471f1669a79221
BigDaveNz <davidjohndudson@gmail.com>
2014-04-05 12:48:23 +1300

    Remove command classes as they are not required in core

--- src/main/test/nz/co/bigdavenz/coreme/chat/CommunicationTest.scala
+++ src/main/test/nz/co/bigdavenz/coreme/chat/CommunicationTest.scala
@@ -7,100 +7,86 @@
 
 package nz.co.bigdavenz.coreme.chat
 
-//
-//import net.minecraft.util.EnumChatFormatting
 
+import net.minecraft.util.EnumChatFormatting
 import org.scalatest.FunSpec
 
-//
-///**
-// * Created by BigDaveNz aka David J. Dudson.
-// * Date 30/03/2014.
-// * Time 4:12 PM.
-// *
-// * Test class for chat
-// */
-//
+
+/**
+ * Created by BigDaveNz aka David J. Dudson.
+ * Date 30/03/2014.
+ * Time 4:12 PM.
+ *
+ * Test class for chat
+ */
+
 class CommunicationTest extends FunSpec {
 
-  // Todo create Dummy Player
-  //
-  //  describe("A Communication to a single player with Error message") {
-  //    val pCom: PlayerCommunication = new PlayerCommunication("TT", "This is a message", CommunicationStyle.ERROR, None)
-  //    it("Should be red") {
-  //      assert(pCom.getStyle.getColor == EnumChatFormatting.RED)
-  //    }
-  //    it("Should be bold") {
-  //      assert(pCom.getStyle.getBold)
-  //    }
-  //    it("Should have a mod initial that's TT") {
-  //      assert(pCom.getModInitial == "TT")
-  //    }
-  //    it("Should have a message \"This is a message\"") {
-  //      assert(pCom.getMessage == "This is a message")
-  //    }
-  //    it("Should not be broadcast") {
-  //      assert(!pCom.getPlayer)
-  //    }
-  //  }
-  //
-  //  describe("A Communication to a single player with Celebration message") {
-  //    val pCom: PlayerCommunication = new PlayerCommunication("TT", "This is a message", CommunicationStyle.CELEBRATION, None)
-  //    it("Should be yellow") {
-  //      assert(pCom.getStyle.getColor == EnumChatFormatting.YELLOW)
-  //    }
-  //    it("Should be bold") {
-  //      assert(pCom.getStyle.getBold)
-  //    }
-  //    it("Should have a mod initial that's TT") {
-  //      assert(pCom.getModInitial == "TT")
-  //    }
-  //    it("Should have a message \"This is a message\"") {
-  //      assert(pCom.getMessage == "This is a message")
-  //    }
-  //    it("Should not be broadcast") {
-  //      assert(!pCom.getPlayer)
-  //    }
-  //  }
-  //
-  //  describe("A Communication to all players with Warning message") {
-  //    val pCom: PlayerCommunication = new PlayerCommunication("TT", "This is a message", CommunicationStyle.WARNING),)
-  //    it("Should be gold") {
-  //      assert(pCom.getStyle.getColor == EnumChatFormatting.GOLD)
-  //    }
-  //    it("Should be bold") {
-  //      assert(pCom.getStyle.getBold)
-  //    }
-  //    it("Should have a mod initial that's TT") {
-  //      assert(pCom.getModInitial == "TT")
-  //    }
-  //    it("Should have a message \"This is a message\"") {
-  //      assert(pCom.getMessage == "This is a message")
-  //    }
-  //    it("Should be broadcast") {
-  //      assert(pCom.getPlayer == )
-  //    }
-  //  }
-  //
-  //  describe("A Communication to all players with Notification message") {
-  //    val pCom: PlayerCommunication = new PlayerCommunication("TS", "This is a message2", CommunicationStyle.NOTIFICATION, true)
-  //    it("Should be blue") {
-  //      assert(pCom.getStyle.getColor == EnumChatFormatting.BLUE)
-  //    }
-  //    it("Should be italic") {
-  //      assert(pCom.getStyle.getItalic)
-  //    }
-  //    it("Should have a mod initial that's TS") {
-  //      assert(pCom.getModInitial == "TS")
-  //    }
-  //    it("Should have a message \"This is a message2\"") {
-  //      assert(pCom.getMessage == "This is a message2")
-  //    }
-  //    it("Should be broadcast") {
-  //      assert(pCom.getPlayer == )
-  //    }
-  //  }
-  //
+  describe("A Communication to a single player with Error message") {
+    val pCom: PlayerCommunication = new PlayerCommunication("TT", "This is a message", CommunicationStyle.ERROR, None)
+    it("Should be red") {
+      assert(pCom.getStyle.getColor == EnumChatFormatting.RED)
+    }
+    it("Should be bold") {
+      assert(pCom.getStyle.getBold)
+    }
+    it("Should have a mod initial that's TT") {
+      assert(pCom.getModInitial == "TT")
+    }
+    it("Should have a message \"This is a message\"") {
+      assert(pCom.getMessage == "This is a message")
+    }
+  }
+
+  describe("A Communication to a single player with Celebration message") {
+    val pCom: PlayerCommunication = new PlayerCommunication("TT", "This is a message", CommunicationStyle.CELEBRATION, None)
+    it("Should be yellow") {
+      assert(pCom.getStyle.getColor == EnumChatFormatting.YELLOW)
+    }
+    it("Should be bold") {
+      assert(pCom.getStyle.getBold)
+    }
+    it("Should have a mod initial that's TT") {
+      assert(pCom.getModInitial == "TT")
+    }
+    it("Should have a message \"This is a message\"") {
+      assert(pCom.getMessage == "This is a message")
+    }
+  }
+
+  describe("A Communication to all players with Warning message") {
+    val pCom: PlayerCommunication = new PlayerCommunication("TT", "This is a message", CommunicationStyle.WARNING, None)
+    it("Should be gold") {
+      assert(pCom.getStyle.getColor == EnumChatFormatting.GOLD)
+    }
+    it("Should be bold") {
+      assert(pCom.getStyle.getBold)
+    }
+    it("Should have a mod initial that's TT") {
+      assert(pCom.getModInitial == "TT")
+    }
+    it("Should have a message \"This is a message\"") {
+      assert(pCom.getMessage == "This is a message")
+    }
+  }
+
+  describe("A Communication to all players with Notification message") {
+    val pCom: PlayerCommunication = new PlayerCommunication("TS", "This is a message2", CommunicationStyle.NOTIFICATION, None)
+    it("Should be blue") {
+      assert(pCom.getStyle.getColor == EnumChatFormatting.BLUE)
+    }
+    it("Should be italic") {
+      assert(pCom.getStyle.getItalic)
+    }
+    it("Should have a mod initial that's TS") {
+      assert(pCom.getModInitial == "TS")
+    }
+    it("Should have a message \"This is a message2\"") {
+      assert(pCom.getMessage == "This is a message2")
+    }
+
+  }
+
   describe("An Error Communication to the console") {
     val cCom: ConsoleCommunication = new ConsoleCommunication("TS", "This is not a message", CommunicationStyle.ERROR)
     it("Should be an Error") {
@@ -113,4 +99,5 @@ class CommunicationTest extends FunSpec {
       assert(cCom.getModInitial == "TS")
     }
   }
+
 }
